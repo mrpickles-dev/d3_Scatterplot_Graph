ecb9ff36b6c6871a904ea2e1752afbcc20bc6587
mrpickles-dev <mr.pickles300@gmail.com>
2021-04-27 17:47:01 +0200

    initial setup

---
+++ script.js
@@ -0,0 +1,74 @@
+
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+let req = new XMLHttpRequest()
+
+let values = [];
+let xScale
+let yScale
+
+
+let width=800
+let height=600
+let padding=40
+
+let svg = d3.selectAll('svg')
+
+let drawCanvas= () => {
+    svg.attr("width", width)
+    svg.attr("height", height)
+       }
+
+let generateScales = () => {
+
+    xScale = d3.scaleLinear()
+    .range([padding, width-padding])
+
+    yScale = d3.scaleTime()
+    .range([padding, height - padding])
+
+}
+
+let drawPoints = () => {
+
+    d3.selectAll('circle')
+    .data(values)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('r', '5')
+    .attr('data-xvalue', (item) => {
+        return item ['Year']
+    })
+    .attr('data-yvalue', (item) => {
+        return new Date(item['Seconds'] * 1000)
+    })
+}
+
+let generateAxes = () => {
+
+    let xAxis = d3.axisBottom(xScale)
+    let yAxis = d3.axisLeft(yScale)
+   
+    svg.append("g")
+    .call(xAxis)
+    .attr("id","x-axis")
+    .attr("transform","translate(0,"+(height-padding)+")")
+
+ svg.append("g")
+    .call(yAxis)
+    .attr("id","y-axis")
+    .attr("transform","translate("+padding+",0)")
+
+}
+
+req.open('GET', url, true)
+req.onload = () => {
+    values = JSON.parse(req.responseText)
+    
+    drawCanvas()
+    generateScales()
+    drawPoints()
+    generateAxes()
+}
+
+req.send()
