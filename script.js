d6d76932baaa1c884d32341fce6e53e2d33df460
mrpickles-dev <mr.pickles300@gmail.com>
2021-04-27 21:24:14 +0200

    update

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let height=600
 let padding=40
 
 let svg = d3.selectAll('svg')
+let tooltip=d3.select("#tooltip")
 
 let drawCanvas= () => {
     svg.attr("width", width)
@@ -20,34 +21,72 @@ let drawCanvas= () => {
 
 let generateScales = () => {
 
-    xScale = d3.scaleLinear()
-    .range([padding, width-padding])
-
-    yScale = d3.scaleTime()
-    .range([padding, height - padding])
-
+    xScale=d3.scaleLinear()
+    .domain([d3.min(values,(items)=>{
+      return items["Year"]
+      })-1,d3.max(values,(items)=>{
+      return items["Year"]
+      })+1])
+     .range([padding,width-padding])
+yScale=d3.scaleTime()
+    .domain([d3.min(values,(item)=>{
+      return new Date(item["Seconds"]*1000)
+      }),d3.max(values,(item)=>{
+      return new Date(item["Seconds"]*1000)
+      })])
+     .range([padding,height-padding])
 }
 
 let drawPoints = () => {
 
-    d3.selectAll('circle')
+    svg.selectAll("circle")
     .data(values)
     .enter()
-    .append('circle')
-    .attr('class', 'dot')
-    .attr('r', '5')
+    .append("circle")
+    .attr("class","dot")
+    .attr("r","5")
     .attr('data-xvalue', (item) => {
         return item ['Year']
     })
     .attr('data-yvalue', (item) => {
         return new Date(item['Seconds'] * 1000)
     })
+    .attr('cx', (item) => {
+        return xScale(item['Year'])
+    })
+    .attr("cy",(item)=>{
+        return yScale(new Date(item["Seconds"]*1000))
+        })
+        
+    .attr("fill",(item)=>{
+        if(item["URL"]===""){
+          return "#FFD166"
+          }else{
+            return "#06D6A0"
+            }
+       })
+     .on("mouseover",(item)=>{
+       tooltip.transition()
+              .style("visibility","visible")
+       if(item["Doping"]!=""){
+         tooltip.text(item["Year"]+"-"+item["Name"]+"-"+item["Time"]+"-"+item["Doping"])
+         }else{
+         tooltip.text(item["Year"]+"-"+item["Name"]+"-"+item["Time"]+"-"+"No Allegations")
+           }
+         tooltip.attr("data-year",item["Year"])
+       })
+     .on("mouseout",(item)=>{
+       tooltip.transition()
+              .style("visibility","hidden")
+       })
 }
 
 let generateAxes = () => {
 
     let xAxis = d3.axisBottom(xScale)
-    let yAxis = d3.axisLeft(yScale)
+    .tickFormat(d3.format('d'))
+   let yAxis=d3.axisLeft(yScale)
+    .tickFormat(d3.timeFormat("%M:%S"))
    
     svg.append("g")
     .call(xAxis)
